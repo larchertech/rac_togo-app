c02d39f775305913d9dcfcf47a1296815ab617e5
larchertech <bokoakoko@gmail.com>
2026-04-30 02:48:08 +0000

    Create AuditLog.php

---
+++ backend/app/Models/AuditLog.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AuditLog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'action',
+        'entite',
+        'entite_id',
+        'data',
+        'ip_address',
+        'user_agent',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
