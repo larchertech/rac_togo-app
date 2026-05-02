8df152084e27f6ba4a648eb7cbe0925779150bbb
larchertech <106818970+larchertech@users.noreply.github.com>
2026-05-02 16:23:06 +0000

    Delete backend directory

--- backend/bootstrap/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
-];
